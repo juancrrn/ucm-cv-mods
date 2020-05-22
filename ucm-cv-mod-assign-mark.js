0b874bd18c89a0a541494fed2ef3c8e492083c56
Juan Carrión <6473248+juancrrn@users.noreply.github.com>
2020-05-22 23:22:17 +0200

    Create ucm-cv-mod-assign-mark.js

---
+++ ucm-cv-mod-assign-mark.js
@@ -0,0 +1,61 @@
+// ==UserScript==
+// @name         Mod de marcas de entregas realizadas CV UCM (ucm-cv-mod-assign-mark)
+// @namespace    https://juancarrion.xyz
+// @version      0.1
+// @description  Mod de marcas de entregas realizadas para el Campus virtual de la Universidad Complutense de Madrid
+// @author       juancrrn
+// @match        https://cv4.ucm.es/moodle/course/*
+// @require      https://code.jquery.com/jquery-3.5.1.slim.min.js
+// ==/UserScript==
+
+var mod = {};
+
+/**
+ * @var {string} storageName Name of the stored assignments cookie.
+ */
+mod.storageName = 'ucm_cv4_stored_submitted_assignments_data_v0.0.1';
+
+/**
+ * Retrieves a client cookie by its name.
+ *
+ * @param {string} name Name of the cookie to retrieve.
+ *
+ * @return {string} Content of the retrieved cookie.
+ */
+mod.readCookie = function (name) {
+    var nameEQ = name + '=';
+
+    var ca = document.cookie.split(';');
+
+    for(var i = 0; i < ca.length; i++) {
+        var c = ca[i];
+        while (c.charAt(0) == ' ') c = c.substring(1, c.length);
+        if (c.indexOf(nameEQ) == 0) return c.substring(nameEQ.length, c.length);
+    }
+
+    return null;
+}
+
+// Wait for the document to be loaded
+$(() => {
+    // Get stored assignments cookie
+    var storedAssignmentsRaw = mod.readCookie(mod.storageName);
+
+    console.log(storedAssignmentsRaw);
+
+    // Parse cookie
+    var storedAssignments = storedAssignmentsRaw.split('-');
+
+    $.each(storedAssignments, function (k, v) {
+        var id = '#module-' + v;
+
+        console.log(id + ' is stored as submitted.');
+
+        var badgeHtml = '<span class="badge badge-success" style="text-indent: 0; vertical-align: middle; margin-right: 5px;">Entregado</span>';
+
+        // Mark the assignments
+        $(badgeHtml).insertBefore(id + ' .instancename');
+
+        $(id + ' .instancename').css('vertical-align', 'middle');
+    });
+});
